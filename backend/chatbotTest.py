0601fe4421d9afa77c01923fd0e537cd4de6feb9
amanster22 <aman.shaik.us@gmail.com>
2025-01-15 15:20:00 -0500

    Update chatbotTest.py

--- backend/chatbotTest.py
+++ backend/chatbotTest.py
@@ -1,20 +1,20 @@
-import openai
+# import openai
 
-# Set up the OpenAI API key
-openai.api_key = "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"
+# # Set up the OpenAI API key
+# openai.api_key = "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"
 
-from openai import OpenAI
-client = OpenAI()
+# from openai import OpenAI
+# client = OpenAI()
 
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "developer", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
+# completion = client.chat.completions.create(
+#     model="gpt-4o",
+#     messages=[
+#         {"role": "developer", "content": "You are a helpful assistant."},
+#         {
+#             "role": "user",
+#             "content": "Write a haiku about recursion in programming."
+#         }
+#     ]
+# )
 
-print(completion.choices[0].message)
+# print(completion.choices[0].message)
